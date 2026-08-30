22c7b68ef90011572081d1420ec841f1ab9b2b55
asokajr388-ship-it <asokajr388@gmail.com>
2026-08-30 21:31:12 +0700

    Delete level.ts

--- level.ts
+++
@@ -1,5 +0,0 @@
-// 1 level HSK naik setiap 100 XP, maksimal HSK 6
-export function calculateHskLevel(xp: number): number {
-  const level = 1 + Math.floor(xp / 100);
-  return Math.min(level, 6);
-}
